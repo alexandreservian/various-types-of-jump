81ec5ded1c5566c714d10122f1a82c48cab3673d
alexandreservian <alexandreservian@gmail.com>
2022-04-12 13:16:11 -0300

    adicionando variacao pelo gravityScale

--- Assets/Scripts/PerfectJump.cs
+++ Assets/Scripts/PerfectJump.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class PerfectJump : MonoBehaviour
 {
     public float jumpHeight = 3f;
+    public float gravityScale = 10f;
+    public float fallingGravityScale = 40f;
     private float jumpForce;
     private Rigidbody2D rb;
     public bool jumping = false;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        jumpForce = Mathf.Sqrt(-2 * jumpHeight * (Physics2D.gravity.y * rb.gravityScale));
+        jumpForce = Mathf.Sqrt(-2 * jumpHeight * (Physics2D.gravity.y * gravityScale));
     }
 
     void Update()
@@ -30,6 +32,12 @@ public class PerfectJump : MonoBehaviour
         if(jumping) {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             jumping = false;
+        }
+
+        if(rb.velocity.y >= 0) {
+            rb.gravityScale = gravityScale;
+        } else if (rb.velocity.y < 0) {
+            rb.gravityScale = fallingGravityScale;
         }
     }
 }
